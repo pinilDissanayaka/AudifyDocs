14253f9f71441baf06554d23d9534f77869fbf31
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-06 17:39:00 +0530

    add requirements

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from st_audiorec import st_audiorec
 import streamlit as st
+from utils.voice_handler import voice_to_text
 
 
 wav_audio_data = st_audiorec()
 
 if wav_audio_data is not None:
     st.audio(wav_audio_data, format='audio/wav')
+    
+    
---
+++ utils/voice_handler.py
@@ -0,0 +1,19 @@
+import os
+import streamlit as st
+from groq import Groq
+
+os.environ['GROQ_API_KEY']=st.secrets['GROQ_API_KEY']
+
+def voice_to_text(voice_file): 
+    client=Groq()
+    
+    transcription = client.audio.transcriptions.create(
+        file=voice_file, # Required audio file
+        model="distil-whisper-large-v3-en", # Required model to use for transcription
+        prompt="Specify context or spelling",  # Optional
+        response_format="json",  # Optional
+        language="en",  # Optional
+        temperature=0.0  # Optional
+    )
+
+    print(transcription.text)
